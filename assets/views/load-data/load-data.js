7533483d6b5ffa41bcacbfd795d9b2f43af10ab6
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2019-08-29 13:08:21 +0700

    update load-data: fix datatables issue with custom http request

--- assets/views/load-data/load-data.js
+++ assets/views/load-data/load-data.js
@@ -12,13 +12,46 @@
             controller: _
         });
 
-    _.$inject = ['$scope', '$state', '$http'];
-    function _($scope, $state, $http) {
+    _.$inject = ['$scope', '$state', 'DTOptionsBuilder', 'DTColumnBuilder'];
+    function _($scope, $state, DTOptionsBuilder, DTColumnBuilder) {
         let $ctrl = this;
         $ctrl.$onInit = () => {
-            $http.get('/assets/test/fileSearch.json').then(res => {
-                let data = res.data;
-            });
+            $scope.dtOptions = DTOptionsBuilder.newOptions()
+                .withOption('ajax', {
+                    url: 'http://cra.tritronik.com:8081/file/search',
+                    dataFilter: (data) => {
+                        data = angular.fromJson(data);
+                        return angular.toJson({
+                            recordsTotal: data.totalElements,
+                            recordsFiltered: data.totalElements,
+                            data: data.content
+                        });
+                    },
+                    dataSrc: 'data',
+                    data: (data) => {
+                        let params = {
+                            limit: data.length,
+                            filename: data.search.value,
+                            page: data.start / data.length
+                        };
+                        if (data.order.length !== 0) {
+                            params['sortBy'] = data.columns[data.order[0].column].data;
+                            params['sortOrder'] = data.order[0].dir;
+                        }
+                        return params;
+                    }
+                })
+                .withOption('processing', true)
+                .withOption('serverSide', true)
+                .withOption('lengthMenu', [5, 10, 20])
+                .withOption('language', { search: 'File Search' })
+                .withPaginationType('simple_numbers');
+            $scope.dtColumns = [
+                DTColumnBuilder.newColumn('uploadDate').withTitle('Upload Date'),
+                DTColumnBuilder.newColumn('filename').withTitle('File Name'),
+                DTColumnBuilder.newColumn('size').withTitle('Size (byte)'),
+                DTColumnBuilder.newColumn('status').withTitle('Status'),
+            ];
         };
 
         $scope.showData = () => {
