58f33f79c02b3c1359897baf43e548cae1af234a
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2019-09-09 19:04:48 +0700

    update dashboard: update view

--- assets/views/dashboard/dashboard.js
+++ assets/views/dashboard/dashboard.js
@@ -10,43 +10,33 @@
             controller: _
         });
 
-    _.$inject = ['$scope', '$timeout', 'FileProcessorService'];
-    function _($scope, $timeout, FileProcessorService) {
+    _.$inject = ['$scope', '$timeout', '$element', 'FileProcessorService'];
+    function _($scope, $timeout, $element, FileProcessorService) {
         var $ctrl = this;
         $ctrl.$onInit = () => {
-            $timeout(async () => {
-                $scope.files = await FileProcessorService.loadFiles(100).then(({ data }) => {
-                    let content = data.content,
-                        totalFiles = content.length,
-                        totalMerged = content.reduce((acc, obj) => acc + obj.mergedRecords, 0),
-                        totalRecords = content.reduce((acc, obj) => acc + obj.totalRecords, 0),
-                        mergedPercentage = totalMerged / totalRecords * 100;
-                    return { totalFiles, totalMerged, totalRecords, mergedPercentage };
-                });
-                $scope.$apply();
+            $scope.id = $scope.$id;
 
-                var datapie = {
-                    labels: ['Processed', 'Unprocessed'],
-                    datasets: [{
-                        data: [$scope.files.mergedPercentage, 100 - $scope.files.mergedPercentage],
-                        backgroundColor: ['#f77eb9', '#7ebcff']
-                    }]
-                };
-
-                var optionpie = {
-                    maintainAspectRatio: false,
-                    responsive: true,
-                    legend: {
-                        display: false,
+            $timeout(() => {
+                let el = $element.find(`#chart-${$scope.id}`),
+                    datapie = {
+                        labels: ['New', 'Processed', 'Completed'],
+                        datasets: [{
+                            data: [20, 20, 60],
+                            backgroundColor: ['#f77eb9', '#fdbd88', '#7ebcff']
+                        }]
                     },
-                    animation: {
-                        animateScale: true,
-                        animateRotate: true
-                    }
-                };
+                    optionpie = {
+                        maintainAspectRatio: false,
+                        responsive: true,
+                        legend: { display: false },
+                        animation: {
+                            animateScale: true,
+                            animateRotate: true
+                        },
+                        tooltips: { enabled: false }
+                    };
 
-                var ctx2 = document.getElementById('chartDonut');
-                var myDonutChart = new Chart(ctx2, {
+                let chart = new Chart(el, {
                     type: 'doughnut',
                     data: datapie,
                     options: optionpie
